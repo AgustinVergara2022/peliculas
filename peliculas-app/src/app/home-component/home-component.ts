2fcf78ff6c4a547ddd4a5a89e78666ff371bb11c
AgustinVergara2022 <agustinvergara215@gmail.com>
2025-10-24 11:53:25 -0300

    mejora visual de inicio con carrusel

--- peliculas-app/src/app/home-component/home-component.ts
+++ peliculas-app/src/app/home-component/home-component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css'
 })
 export class HomeComponent {
+   constructor(private router: Router) {}
 
+  verDetalle(imdbID: string) {
+    this.router.navigate(['/pelicula', imdbID]);
+  }
 }
